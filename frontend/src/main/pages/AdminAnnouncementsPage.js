b1eb45c5aa57086d2c1199d500e5067614bde282
Jason Nguyen <jasontnguyen39@gmail.com>
2024-05-17 22:13:59 -0700

    jn - added heading to Announcements page that displays the correct commons name

--- frontend/src/main/pages/AdminAnnouncementsPage.js
+++ frontend/src/main/pages/AdminAnnouncementsPage.js
@@ -1,13 +1,31 @@
 
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useBackend} from "main/utils/useBackend";
+import { useParams } from "react-router-dom";
 
 export default function AdminAnnouncementsPage() {
+    let { commonsId } = useParams();
+
+    // Stryker disable all
+    const { data: commonsPlus } = useBackend(
+        [`/api/commons/plus?id=${commonsId}`],
+        {
+            method: "GET",
+            url: "/api/commons/plus",
+            params: {
+                id: commonsId,
+            },
+        }
+    );
+    const name = commonsPlus?.commons.name;
+
     return (
         <BasicLayout>
             <div className="pt-2">
-                <h1>Feature Coming Soon</h1>
+                <h1>Announcements for {name}</h1>
             </div>
         </BasicLayout>
     )
+    
 }
