c0660f3e82a9331edcc2760b836bba2ddcf498f9
Jason Nguyen <jasontnguyen39@gmail.com>
2024-05-25 16:52:31 -0700

    jn - fixed navigation path for edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -60,7 +60,7 @@ function App() {
                 element={<AdminCreateAnnouncementsPage />}
             />
             <Route
-                path="/admin/announcements/:commonsId/edit"
+                path="/announcements/edit/:commonsId"
                 element={<AdminEditAnnouncementsPage />}
             />
         </>
