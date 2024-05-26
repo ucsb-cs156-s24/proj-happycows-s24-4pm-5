38ba91029b74fbcc22817456f32a1924938b5008
nevetsle <stevenle@ucsb.edu>
2024-05-25 17:10:35 -0700

    Deleted unused vars

--- frontend/src/tests/pages/PlayPage.test.js
+++ frontend/src/tests/pages/PlayPage.test.js
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
