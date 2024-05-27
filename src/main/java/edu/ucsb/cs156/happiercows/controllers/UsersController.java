b1fb12dc29991e9712706709a1a69a0c7c510dcc
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-05-26 23:46:21 -0700

    WW-add imports for suspend and restore

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/UsersController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/UsersController.java
@@ -9,11 +9,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.ucsb.cs156.happiercows.entities.User;
 import edu.ucsb.cs156.happiercows.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @Tag(name="User information (admin only)")
 @RequestMapping("/api/admin/users")
