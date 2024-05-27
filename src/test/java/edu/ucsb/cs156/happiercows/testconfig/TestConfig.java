ef73542912482f2e693c34095be54c8b3641dfa8
nevetsle <stevenle@ucsb.edu>
2024-05-27 02:35:39 -0700

    Upgraded to Spring-3

--- src/test/java/edu/ucsb/cs156/happiercows/testconfig/TestConfig.java
+++ src/test/java/edu/ucsb/cs156/happiercows/testconfig/TestConfig.java
@@ -1,12 +1,15 @@
 package edu.ucsb.cs156.happiercows.testconfig;
 
+import edu.ucsb.cs156.happiercows.config.SecurityConfig;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import edu.ucsb.cs156.happiercows.services.CurrentUserService;
 import edu.ucsb.cs156.happiercows.services.GrantedAuthoritiesService;
+import org.springframework.context.annotation.Import;
 
 @TestConfiguration
+@Import(SecurityConfig.class)
 public class TestConfig {
 
     @Bean
