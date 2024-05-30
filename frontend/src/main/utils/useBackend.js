6264e1f2b9d1f6319c9c4dc9901e4f3203049045
nevetsle <stevenle@ucsb.edu>
2024-05-29 22:04:31 -0700

    Disabled toast notifications, switched to console

--- frontend/src/main/utils/useBackend.js
+++ frontend/src/main/utils/useBackend.js
@@ -49,6 +49,30 @@ export function useBackend(queryKey, axiosParameters, initialData, rest) {
         });
 }
 
+export function useBackendConsole(queryKey, axiosParameters, initialData, rest) {
+
+    return useQuery({
+        queryKey: queryKey,
+        queryFn: async () => {
+            try {
+                const response = await axios(axiosParameters);
+                return response.data;
+            } catch (e) {
+                // Stryker disable next-line OptionalChaining
+                if (e.response?.data?.message) {
+                    console.log(e.response.data.message);
+                } else {
+                    const errorMessage = `Error communicating with backend via ${axiosParameters.method} on ${axiosParameters.url}`;
+                    toast.error(errorMessage);
+                }
+                throw e;
+            }
+        }, 
+        initialData: initialData,
+        ...rest
+        });
+}
+
 const wrappedParams = async (params) => {
     return await (await axios(params)).data;
 };
@@ -76,4 +100,3 @@ export function useBackendMutation(objectToAxiosParams, useMutationParams, query
         ...useMutationParams
     })
 }
-
