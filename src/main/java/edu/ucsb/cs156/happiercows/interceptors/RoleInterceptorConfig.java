393e2dc05be466de0d38b30f15adef1b032e8745
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-05-30 15:07:05 -0700

    WW-create RoleInterceptorConfig file

---
+++ src/main/java/edu/ucsb/cs156/happiercows/interceptors/RoleInterceptorConfig.java
@@ -0,0 +1,18 @@
+package edu.ucsb.cs156.happiercows.interceptors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class RoleUserInterceptorAppConfig implements WebMvcConfigurer {
+   @Autowired
+   RoleUserInterceptor roleUserInterceptor;
+
+   @Override
+   public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(roleUserInterceptor);
+   }
+
+}
