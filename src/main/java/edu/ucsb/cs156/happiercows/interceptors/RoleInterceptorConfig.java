a7c90a4feda51b52ba225a22f5bd96827b299d03
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-06-02 16:59:41 -0700

    WW-update config variable

--- src/main/java/edu/ucsb/cs156/happiercows/interceptors/RoleInterceptorConfig.java
+++ src/main/java/edu/ucsb/cs156/happiercows/interceptors/RoleInterceptorConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-public class RoleUserInterceptorAppConfig implements WebMvcConfigurer {
+public class RoleInterceptorAppConfig implements WebMvcConfigurer {
    @Autowired
-   RoleUserInterceptor roleUserInterceptor;
+   RoleInterceptor roleInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
-      registry.addInterceptor(roleUserInterceptor);
+      registry.addInterceptor(roleInterceptor);
    }
 
 }
