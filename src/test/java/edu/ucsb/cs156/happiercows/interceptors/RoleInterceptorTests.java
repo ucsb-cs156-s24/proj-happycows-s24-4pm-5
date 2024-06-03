7c2939f2455e939440fe44700cc9c9285b6143a1
winstonwangUCSB <91697899+winstonwangUCSB@users.noreply.github.com>
2024-06-02 17:12:12 -0700

    WW-update imports for tests

--- src/test/java/edu/ucsb/cs156/happiercows/interceptors/RoleInterceptorTests.java
+++ src/test/java/edu/ucsb/cs156/happiercows/interceptors/RoleInterceptorTests.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs156.happycows.interceptors;
+package edu.ucsb.cs156.happiercows.interceptors;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
